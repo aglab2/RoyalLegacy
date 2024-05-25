95b7dc6b3f6772a28ca19c8b3b07d58b9546f771
aglab2 <aglab3@gmail.com>
2024-05-25 21:41:02 +0800

    Fixed BOB warps, fixed warps crashing the game

---
+++ levels/bob/area_3/geo.inc.c
@@ -0,0 +1,38 @@
+#include "src/game/envfx_snow.h"
+
+const GeoLayout bob_area_3_geo[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_ASM(0, geo_envfx_main),
+		GEO_ASM(20480, geo_movtex_draw_water_regions),
+		GEO_ASM(20481, geo_movtex_draw_water_regions),
+		GEO_ASM(20482, geo_movtex_draw_water_regions),
+		GEO_ASM(0, geo_cannon_circle_base),
+	GEO_CLOSE_NODE(),
+	GEO_RETURN(),
+};
+const GeoLayout bob_area_3[] = {
+	GEO_NODE_SCREEN_AREA(10, SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SCREEN_WIDTH/2, SCREEN_HEIGHT/2),
+	GEO_OPEN_NODE(),
+		GEO_ZBUFFER(0),
+		GEO_OPEN_NODE(),
+			GEO_NODE_ORTHO(100.0000),
+			GEO_OPEN_NODE(),
+				GEO_BACKGROUND_COLOR(0x0001),
+			GEO_CLOSE_NODE(),
+		GEO_CLOSE_NODE(),
+		GEO_ZBUFFER(1),
+		GEO_OPEN_NODE(),
+			GEO_CAMERA_FRUSTUM_WITH_FUNC(45.0000, 100, 30000, geo_camera_fov),
+			GEO_OPEN_NODE(),
+				GEO_CAMERA(1, 0, 0, 0, 0, -100, 0, geo_camera_main),
+				GEO_OPEN_NODE(),
+					GEO_BRANCH(1, bob_area_3_geo),
+					GEO_RENDER_OBJ(),
+					GEO_ASM(ENVFX_MODE_NONE, geo_envfx_main),
+				GEO_CLOSE_NODE(),
+			GEO_CLOSE_NODE(),
+		GEO_CLOSE_NODE(),
+	GEO_CLOSE_NODE(),
+	GEO_END(),
+};
