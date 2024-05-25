ff95a7a5a3c63d2a753cabc86003ba972db017f5
aglab2 <aglab3@gmail.com>
2024-05-25 22:08:50 +0800

    Added music

--- levels/entry.c
+++ levels/entry.c
@@ -9,7 +9,7 @@
 
 #include "config.h"
 
-#define TEST_LEVEL LEVEL_BOB
+#define TEST_LEVEL LEVEL_CASTLE_GROUNDS
 
 extern const LevelScript level_main_scripts_entry[];
 const LevelScript level_script_entry[] = {
