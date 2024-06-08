5dbc44582381f8c000745eefd9333122dbaf961f
aglab2 <aglab3@gmail.com>
2024-06-08 17:23:17 +0800

    Applied all transforms in C2

--- levels/wf/texscroll.inc.c
+++ levels/wf/texscroll.inc.c
@@ -19,6 +19,48 @@ void scroll_wf_dl_WinterLevel_2020version_001_mesh_layer_5_vtx_0() {
 	currentY += deltaY;
 }
 
+void scroll_wf_dl_zzWinterLevel_2020version_013_mesh_layer_0_vtx_0() {
+	int i = 0;
+	int count = 281;
+	int width = 64 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(wf_dl_zzWinterLevel_2020version_013_mesh_layer_0_vtx_0);
+
+	deltaX = (int)(0.1599999964237213 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
+void scroll_wf_dl_zzWinterLevel_2020version_013_mesh_layer_0_vtx_1() {
+	int i = 0;
+	int count = 103;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(wf_dl_zzWinterLevel_2020version_013_mesh_layer_0_vtx_1);
+
+	deltaY = (int)(0.12999999523162842 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
 void scroll_gfx_wf_dl_WinterLevel_2020version_001_mesh_layer_5() {
 	Gfx *mat = segmented_to_virtual(wf_dl_WinterLevel_2020version_001_mesh_layer_5);
 
@@ -28,5 +70,7 @@ void scroll_gfx_wf_dl_WinterLevel_2020version_001_mesh_layer_5() {
 
 void scroll_wf() {
 	scroll_wf_dl_WinterLevel_2020version_001_mesh_layer_5_vtx_0();
+	scroll_wf_dl_zzWinterLevel_2020version_013_mesh_layer_0_vtx_0();
+	scroll_wf_dl_zzWinterLevel_2020version_013_mesh_layer_0_vtx_1();
 	scroll_gfx_wf_dl_WinterLevel_2020version_001_mesh_layer_5();
 };
--- levels/wf/texscroll.inc.h
+++ levels/wf/texscroll.inc.h
@@ -1,3 +1,5 @@
 extern void scroll_wf_dl_WinterLevel_2020version_001_mesh_layer_5_vtx_0();
+extern void scroll_wf_dl_zzWinterLevel_2020version_013_mesh_layer_0_vtx_0();
+extern void scroll_wf_dl_zzWinterLevel_2020version_013_mesh_layer_0_vtx_1();
 extern void scroll_gfx_wf_dl_WinterLevel_2020version_001_mesh_layer_5();
 extern void scroll_wf();
