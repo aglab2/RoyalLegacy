cf426f606464d339e8b008a33a28d04a325f21c0
aglab2 <aglab3@gmail.com>
2024-07-06 17:52:10 +0800

    Attempt to raise pink bobomb draw distance

--- src/game/behaviors/bobomb.inc.c
+++ src/game/behaviors/bobomb.inc.c
@@ -305,6 +305,7 @@ void bhv_bobomb_buddy_init(void) {
     o->oFriction = 0.8f;
     o->oBuoyancy = 1.3f;
     o->oInteractionSubtype = INT_SUBTYPE_NPC;
+    o->oDrawingDistance = 30000.0f;
 }
 
 void bobomb_buddy_act_idle(void) {
