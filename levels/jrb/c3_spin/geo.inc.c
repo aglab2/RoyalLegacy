c771fe5bf512103d31b8b0be96c5b87766a5028d
aglab2 <aglab3@gmail.com>
2024-07-08 13:23:53 +0800

    Raise culling radius of c3 spinning things

--- levels/jrb/c3_spin/geo.inc.c
+++ levels/jrb/c3_spin/geo.inc.c
@@ -2,7 +2,7 @@
 
 extern Gfx *geo_update_layer_transparency_envcolor2(s32 callContext, struct GraphNode *node, UNUSED void *context);
 const GeoLayout c3_spin_geo[] = {
-	GEO_NODE_START(),
+	GEO_CULLING_RADIUS(1000),
 	GEO_OPEN_NODE(),
 		GEO_ASM(0, geo_update_layer_transparency_envcolor2),
 		GEO_OPEN_NODE(),
