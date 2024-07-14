0c8ae13fb5ab7d0a122d9f9204391498fd85582c
aglab2 <aglab3@gmail.com>
2024-07-14 20:57:54 +0800

    Fixed swapped collision

--- src/game/behaviors/aglab_epic_door.inc.c
+++ src/game/behaviors/aglab_epic_door.inc.c
@@ -5,10 +5,10 @@ void bhv_epic_door()
 {
     if (0 == o->oBehParams2ndByte)
     {
-        obj_set_collision_data(o, beta_door_r_collision);
+        obj_set_collision_data(o, beta_door_l_collision);
     }
     else
     {
-        obj_set_collision_data(o, beta_door_l_collision);
+        obj_set_collision_data(o, beta_door_r_collision);
     }
 }
