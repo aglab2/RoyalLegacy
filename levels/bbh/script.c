dc19e3421bf938153bae98222e0bb70a2d4e7f14
aglab2 <aglab3@gmail.com>
2024-07-21 19:50:04 +0800

    Fixed music in beta worlds

--- levels/bbh/script.c
+++ levels/bbh/script.c
@@ -55,7 +55,7 @@ const LevelScript level_bbh_entry[] = {
 		OBJECT(22, 2098, 48, -1894, 0, 0, 0, 0x50000, bhvWarpPipe),
 		TERRAIN(bbh_area_1_collision),
 		MACRO_OBJECTS(bbh_area_1_macro_objs),
-		SET_BACKGROUND_MUSIC(0x00, 33),
+		SET_BACKGROUND_MUSIC(0x00, 0x31),
 		TERRAIN_TYPE(TERRAIN_GRASS),
 		/* Fast64 begin persistent block [area commands] */
 		/* Fast64 end persistent block [area commands] */
