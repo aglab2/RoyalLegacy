51dd64ffb80f866023f00cb9ba05b13bcd589b8f
aglab2 <aglab3@gmail.com>
2024-07-21 19:54:46 +0800

    Added updated hub

--- levels/castle_grounds/texscroll.inc.c
+++ levels/castle_grounds/texscroll.inc.c
@@ -61,6 +61,27 @@ void scroll_castle_grounds_dl_HubWorld_011_mesh_layer_5_vtx_0() {
 	currentX += deltaX;
 }
 
+void scroll_castle_grounds_dl_zsplash_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 12;
+	int width = 64 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_zsplash_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.30000001192092896 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
 void scroll_gfx_castle_grounds_dl_HubWorld_001_mesh_layer_1() {
 	Gfx *mat = segmented_to_virtual(castle_grounds_dl_HubWorld_001_mesh_layer_1);
 
@@ -79,6 +100,7 @@ void scroll_castle_grounds() {
 	scroll_castle_grounds_dl_HubWorld_001_mesh_layer_1_vtx_0();
 	scroll_castle_grounds_dl_HubWorld_002_mesh_layer_1_vtx_6();
 	scroll_castle_grounds_dl_HubWorld_011_mesh_layer_5_vtx_0();
+	scroll_castle_grounds_dl_zsplash_mesh_layer_5_vtx_0();
 	scroll_gfx_castle_grounds_dl_HubWorld_001_mesh_layer_1();
 	scroll_gfx_castle_grounds_dl_HubWorld_011_mesh_layer_5();
 };
--- levels/castle_grounds/texscroll.inc.h
+++ levels/castle_grounds/texscroll.inc.h
@@ -1,6 +1,7 @@
 extern void scroll_castle_grounds_dl_HubWorld_001_mesh_layer_1_vtx_0();
 extern void scroll_castle_grounds_dl_HubWorld_002_mesh_layer_1_vtx_6();
 extern void scroll_castle_grounds_dl_HubWorld_011_mesh_layer_5_vtx_0();
+extern void scroll_castle_grounds_dl_zsplash_mesh_layer_5_vtx_0();
 extern void scroll_gfx_castle_grounds_dl_HubWorld_001_mesh_layer_1();
 extern void scroll_gfx_castle_grounds_dl_HubWorld_011_mesh_layer_5();
 extern void scroll_castle_grounds();
