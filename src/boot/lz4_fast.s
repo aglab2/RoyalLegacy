bc5a61042f28747526a85c7f2fc061bbb811cc3c
aglab2 <aglab3@gmail.com>
2024-08-05 21:47:08 +0800

    Added LZ4 fixup slow path

--- src/boot/lz4_fast.s
+++ src/boot/lz4_fast.s
@@ -15,7 +15,7 @@
 #define match_off   $s3
 #define match_len   $s4
 #define token       $s5
-#define outbuf_orig $s6
+#define offsets     $s6
 #define v0_st       $s7
 
 #define dma_ctx     $s8
@@ -37,9 +37,9 @@ decompress_lz4_full_fast:
     sw $s3, 0x24($sp)
     sw $s4, 0x28($sp)
     sw $s5, 0x2C($sp)
-    sw $s6, 0x30($sp)
-    sw $s7, 0x34($sp)
-    sw $s8, 0x38($sp)
+    sw $s7, 0x30($sp)
+    sw $s8, 0x34($sp)
+    sd $s6, 0x38($sp)
 
     move $s0, $a0
     move $s1, $a1
@@ -47,7 +47,10 @@ decompress_lz4_full_fast:
     move dma_ctx, $a3
 
     add $s1, $s0                                # calculate end of input buffer
-    move outbuf_orig, outbuf
+    li offsets, 0x01210444                      # inc32table compressed
+    dsll offsets, offsets, 32
+    li $t0, 0x000fc123                          # dec64table compressed
+    or offsets, $t0                             # offsets have combined inc32table+dec64table
     move dma_ptr, $a0
     addiu dma_ptr, dma_ptr, -16
 
@@ -92,7 +95,9 @@ decompress_lz4_full_fast:
      addiu match_len, MINMATCH                  # add implicit MINMATCH to match length
 
 .Lmatch:
-    blt match_off, match_len, .Lmatch1_loop_check   # check if we can do 8-byte copy
+    blt match_off, match_len, .Lslow_path       # check if we can do 8-byte copy
+
+.Lwild_copy:
      sub v0_st, outbuf, match_off                 # calculate start of match
 .Lmatch8_loop:                                  # 8-byte copy loop
     ldl $t0, 0(v0_st)                             # load 8 bytes
@@ -100,6 +105,7 @@ decompress_lz4_full_fast:
     addiu v0_st, 8
     sdl $t0, 0(outbuf)                          # store 8 bytes
     sdr $t0, 7(outbuf)
+.Lfixuped_wild_copy:
     addiu match_len, -8
     bgtz match_len, .Lmatch8_loop               # check we went past match_len
      addiu outbuf, 8
@@ -122,17 +128,29 @@ decompress_lz4_full_fast:
     b .Lloop                                    # jump to main loop
      addu outbuf, match_len                     # adjust pointer remove extra bytes
 
-.Lmatch1_loop_check:                            # 1-byte copy loop
+.Lslow_path:                                    # We are on a slow potentially, check offsets
     beq match_off, 1, .Lmatch1_memset           # if match_off is 1, it's a memset
-.Lmatch1_loop:                                  # 1-byte copy loop
     lbu $t0, 0(v0_st)                             # load 1 byte
-    addiu v0_st, 1
-    sb $t0, 0(outbuf)                           # store 1 byte
-    addiu match_len, -1
-    bgtz match_len, .Lmatch1_loop               # check we went past match_len
-     addiu outbuf, 1
-    b .Lloop                                    # jump to main loop
-     nop
+    slti $t9, match_off, 8                      # Check if offset is >=8, in this case can still fast path 
+    beqz $t9, .Lmatch8_loop                     # the chance of this condition is incredibly low
+    sll $t3, match_off, 2                       # from here on, offset is between 2 and 7
+    lbu $t1, 1(v0_st)                           # converge this case to fast memcpy, unroll copy 4 bytes
+    sb $t0, 0(outbuf)
+    sb $t1, 1(outbuf)
+    lbu $t0, 2(v0_st)
+    lbu $t1, 3(v0_st)
+    sb $t0, 2(outbuf)
+    sb $t1, 3(outbuf)
+    dsllv $t4, offsets, $t3                     # fetch an offset from 'offsets' constant
+    dsra $t5, $t4, 60                          # $t5 is inc32table[offset]
+    add v0_st, v0_st, $t5
+    lwl $t0, 0(v0_st)
+    sra $t6, $t4, 28                           # $t6 is dec64table[offset]
+    lwr $t0, 3(v0_st)
+    sub v0_st, v0_st, $t6
+    swl $t0, 4(outbuf)
+    b .Lfixuped_wild_copy
+    swr $t0, 7(outbuf)
 
 .Lend:
     lw $ra, 0x14($sp)
@@ -142,9 +160,9 @@ decompress_lz4_full_fast:
     lw $s3, 0x24($sp)
     lw $s4, 0x28($sp)
     lw $s5, 0x2C($sp)
-    lw $s6, 0x30($sp)
-    lw $s7, 0x34($sp)
-    lw $s8, 0x38($sp)
+    lw $s7, 0x30($sp)
+    lw $s8, 0x34($sp)
+    ld $s6, 0x38($sp)
     jr $ra
     addiu $sp, $sp, 0x40
 
