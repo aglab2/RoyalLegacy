7991b65dab32d27efc3b943c8b4f46f58fb066a8
aglab2 <aglab3@gmail.com>
2024-08-10 12:33:19 +0800

    Fixed SA

--- levels/sa/texscroll.inc.c
+++ levels/sa/texscroll.inc.c
@@ -61,6 +61,52 @@ void scroll_sa_dl_zone3_extra_cull_2_mesh_layer_1_vtx_2() {
 	currentY += deltaY;
 }
 
+void scroll_sa_dl_zone4_door_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 12;
+	int width = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int timeX;
+	float amplitudeX = 0.5;
+	float frequencyX = 0.5;
+	float offsetX = 0.0;
+	Vtx *vertices = segmented_to_virtual(sa_dl_zone4_door_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(amplitudeX * frequencyX * coss((frequencyX * timeX + offsetX) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;	timeX += 1;
+}
+
+void scroll_sa_dl_zone4_door_inside_mesh_layer_1_vtx_0() {
+	int i = 0;
+	int count = 10;
+	int width = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(sa_dl_zone4_door_inside_mesh_layer_1_vtx_0);
+
+	deltaX = (int)(0.5 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
 void scroll_sa_dl_zone4water_mesh_layer_5_vtx_0() {
 	int i = 0;
 	int count = 21;
@@ -107,6 +153,8 @@ void scroll_sa() {
 	scroll_sa_dl_zone1water_mesh_layer_5_vtx_0();
 	scroll_sa_dl_zone3_mesh_layer_1_vtx_7();
 	scroll_sa_dl_zone3_extra_cull_2_mesh_layer_1_vtx_2();
+	scroll_sa_dl_zone4_door_mesh_layer_5_vtx_0();
+	scroll_sa_dl_zone4_door_inside_mesh_layer_1_vtx_0();
 	scroll_sa_dl_zone4water_mesh_layer_5_vtx_0();
 	scroll_sa_dl_skybox4sh_mesh_layer_0_vtx_0();
 };
--- levels/sa/texscroll.inc.h
+++ levels/sa/texscroll.inc.h
@@ -1,6 +1,8 @@
 extern void scroll_sa_dl_zone1water_mesh_layer_5_vtx_0();
 extern void scroll_sa_dl_zone3_mesh_layer_1_vtx_7();
 extern void scroll_sa_dl_zone3_extra_cull_2_mesh_layer_1_vtx_2();
+extern void scroll_sa_dl_zone4_door_mesh_layer_5_vtx_0();
+extern void scroll_sa_dl_zone4_door_inside_mesh_layer_1_vtx_0();
 extern void scroll_sa_dl_zone4water_mesh_layer_5_vtx_0();
 extern void scroll_sa_dl_skybox4sh_mesh_layer_0_vtx_0();
 extern void scroll_sa();
