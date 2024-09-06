7faa9c62314b43bc324d9386692b1c6c61ea240a
Gitboy-2000 <romansuperduper@gmail.com>
2024-09-06 21:21:54 +1000

    cactus-changess

--- levels/sa/breakwindow/geo_header.h
+++ levels/sa/breakwindow/geo_header.h
@@ -1,5 +1,6 @@
 extern const GeoLayout breakwindow_geo[];
-extern u8 breakwindow_Windows_rgba16[];
+extern u8 breakwindow_Windows_ci8[];
+extern u8 breakwindow_Windows_pal_rgba16[];
 extern Vtx breakwindow_window_mesh_layer_1_vtx_0[4];
 extern Gfx breakwindow_window_mesh_layer_1_tri_0[];
 extern Gfx mat_breakwindow_Windows_f3d[];
